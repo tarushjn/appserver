7b519206c005648b8ad17f1c8b2c35a385fadf6c
Saurabh nagpal <saurabhnagpal@Saurabhs-MacBook-Air.local>
2017-07-17 12:14:24 +0530

    some refactor

--- src/main/java/com/jwt/controller/ImageController.java
+++ src/main/java/com/jwt/controller/ImageController.java
@@ -97,8 +97,7 @@ public class ImageController {
 	}
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	@ResponseBody
-	public List<PutObjectResult> upload(@RequestParam("file") MultipartFile[] multipartFiles) {
+	public  List<PutObjectResult> upload(@RequestParam("file") MultipartFile[] multipartFiles) {
 		return s3Wrapper.upload(multipartFiles);
 		}
 
